3da71f8d0826f41caa72b34ad3d3bb6424c3ec25
GinaFabienne <ginafabienne@gmail.com>
2021-06-08 16:03:10 +0200

    length of a string

--- day1.py
+++ day1.py
@@ -12,3 +12,11 @@ print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello" + "world")')
 print("New lines can be created with a backslash and n.")
+
+#input function
+input("A prompt for the user")
+print ("Hello " +input("What is your name? ")) 
+
+#length of a string
+name1 = input("What is your name? ")
+print(len(name1)) 
