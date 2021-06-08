dea94a4eb6690fc8520bf018cc94af7b193e6a18
GinaFabienne <ginafabienne@gmail.com>
2021-06-08 15:14:13 +0200

    first exercises

---
+++ day1.py
@@ -0,0 +1,14 @@
+#Day 1
+#Printing to the console in Python
+print("Hello World")
+print("im learning python")
+
+#String Manipulation and Code Intelligence
+print("Hello World!\nHello World!")
+print("Hello" + " " + "Gina")
+
+#Debugging
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello" + "world")')
+print("New lines can be created with a backslash and n.")
