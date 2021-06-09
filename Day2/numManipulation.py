fc8583fef7a6cf98e4fad2001f6b79aeec0ba179
GinaFabienne <ginafabienne@gmail.com>
2021-06-09 21:50:40 +0200

    day2

---
+++ Day2/numManipulation.py
@@ -0,0 +1,28 @@
+#rounding numbers
+
+print(round(8 / 3))
+
+#if you want to specify the number of decimal places to round, like so
+print(round(8 / 3, 2))
+
+#floor division, because everytime a number is divided it gets turned into a floating point number,
+#floor division will automatically turn it into an int
+print(type(8 // 3))
+
+
+#Adding 1 
+score = 0
+height = 1.8
+
+score += 1
+print(score)
+
+#minusing -=
+
+#when there are different data types -CONVERSION
+#f-string
+#f must go in front of the string
+print(f"you score is {score}, your height is {height}")
+
+#this converted all different data types into a string. 
+
